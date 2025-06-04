7d0ab8169eb44b107119b68aee93299cfcc58583
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-03 19:55:26 -0700

    green

--- modules/pi_estimator.py
+++ modules/pi_estimator.py
@@ -4,6 +4,8 @@ import numpy as np
 
 
 def python_estimate_pi(samples: int) -> float:
+    if samples <= 0:
+        raise ValueError("Samples may not be less than or equal to 0")
     x = np.random.uniform(-1.0, 1.0, samples)
     y = np.random.uniform(-1.0, 1.0, samples)
     inside_circle = (x**2 + y**2) <= 1.0
